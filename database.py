8c512ee75725b75143e5cd5c797d514c82f4d7f3
nicociri <basuradependragon@gmail.com>
2024-06-07 13:54:48 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, String, Integer, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgres://whatsautodb_user:9VVfAXKuQzO4RYYbtOMds4AyFtj43LsT@dpg-cphhlau3e1ms73d8kqh0-a/whatsautodb"
+DATABASE_URL = "postgresql://whatsautodb_user:9VVfAXKuQzO4RYYbtOMds4AyFtj43LsT@dpg-cphhlau3e1ms73d8kqh0-a/whatsautodb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
